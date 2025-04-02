d98a38f7def0ebff6810f007a138287c293ba116
Malik Peterson <malik.peterson@Mac-J0MCPJHWKL.local>
2025-04-01 20:20:44 -0400

    Finish marketing page layout and heading component

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,14 @@
+import Heading from "./_components/heading";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="max-w-3xl text-center space-y-8 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
+  
